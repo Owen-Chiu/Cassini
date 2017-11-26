989ba84125922cd464712588c0e8ff85f5f44199
Adriana <adrianammassie@gmail.com>
2017-11-26 00:54:52 -0500

    Added gitignore

--- Adriana_Bot.js
+++ Adriana_Bot.js
@@ -93,21 +93,21 @@ if(!message.content.startsWith(prefix)) return;
 
 
   if(message.content.startsWith(prefix + "MemeSong") || message.content.startsWith(prefix + "memesong")){
-      var Array = [
-        'https://youtu.be/RxbqKU4j1fU',
-        'https://youtu.be/Je_8A5hurSY',
-        'https://youtu.be/zPqYVBFtBkw',
-        'https://youtu.be/vcaPiiFZu2o',
-        'https://youtu.be/u_ZtiOWdVHU',
-        'https://youtu.be/lYbGEfoOUbs',
-        'https://youtu.be/3H2hWgVbblw',
-        'https://youtu.be/nDN7M0J3HXc',
-        'https://youtu.be/ZZ5LpwO-An4',
-        'https://youtu.be/Byt2D_8acz0'
-      ];
-      var Meme = Math.floor(Math.random()*Array.length);
-      message.channel.send(`${Array[Meme]}`)
-    }
+    var Array = [
+      'https://youtu.be/RxbqKU4j1fU',
+      'https://youtu.be/Je_8A5hurSY',
+      'https://youtu.be/zPqYVBFtBkw',
+      'https://youtu.be/vcaPiiFZu2o',
+      'https://youtu.be/u_ZtiOWdVHU',
+      'https://youtu.be/lYbGEfoOUbs',
+      'https://youtu.be/3H2hWgVbblw',
+      'https://youtu.be/nDN7M0J3HXc',
+      'https://youtu.be/ZZ5LpwO-An4',
+      'https://youtu.be/Byt2D_8acz0'
+    ];
+    var Meme = Math.floor(Math.random()*Array.length);
+    message.channel.send(`${Array[Meme]}`)
+  }
 
   
     
